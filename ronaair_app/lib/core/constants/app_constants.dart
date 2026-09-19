59f9bda1c2fa092f1d49fdf96104c40bb55ce54e
isajahwa <isajahwa3131@gmail.com>
2026-09-20 01:00:48 +0700

    perbaikan

--- ronaair_app/lib/core/constants/app_constants.dart
+++ ronaair_app/lib/core/constants/app_constants.dart
@@ -10,7 +10,7 @@ class AppConstants {
   // karena localhost di emulator = emulator sendiri.
   // Contoh: 'http://10.0.2.2:8080'
   // Untuk device fisik, ganti dengan IP laptop di jaringan lokal.
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'http://192.168.56.1:8080';
 
   // Versi API
   static const String apiVersion = 'v1';
